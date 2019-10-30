cc6e35cacfba762899be86587ff5ccd3ee615c20
Kristin Myers <kristinmyers@outlook.com>
2019-10-30 15:35:25 -0400

    Attempt at 3b

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 // It is always helpful to use comments in your code!
+//Print 212 inside of span with ID boiling-temp
+/*const boilingTemp = document.getElementById('boiling-temp');
+//console.log(boilingTemp);
+const printBoiling = () => {
+  document.createTextNode('212');
+};
+console.log(printBoiling);*/
+
+var message1 = document.createTextNode('212');
+//console.log(message1);
+//console.log(boilingTemp);
+var boilingTemp = document.getElementById('boiling-temp');
+message1.appendChild(boilingTemp);
