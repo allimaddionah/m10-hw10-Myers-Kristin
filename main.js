d6fd04e39d921ffe620a16662d0e876e001b29d4
Kristin Myers <kristinmyers@outlook.com>
2019-11-02 18:12:03 -0400

    Add javascript code after code along

--- main.js
+++ main.js
@@ -7,8 +7,50 @@ const printBoiling = () => {
 };
 console.log(printBoiling);*/
 
-var message1 = document.createTextNode('212');
-//console.log(message1);
-//console.log(boilingTemp);
-var boilingTemp = document.getElementById('boiling-temp');
-message1.appendChild(boilingTemp);
+// var message1 = document.createTextNode('212');
+// //console.log(message1);
+// //console.log(boilingTemp);
+// var boilingTemp = document.getElementById('boiling-temp');
+// message1.appendChild(boilingTemp);
+
+//Code along
+
+const boilingTemp = document.getElementById('boiling-temp');
+const freezingTemp = document.getElementById('freezing-temp');
+const waterTemp = document.getElementById('water-temp');
+
+const boilingMessage = document.getElementById('boiling');
+const freezingMessage = document.getElementById('frozen');
+const goodTempMessage = document.getElementById('good-temp');
+
+const frozenNumber = 32;
+const boilingNumber = 212;
+const waterTempNumber = 72;
+
+const showBoilingTemp = () => {
+  freezingTemp.innerHTML = frozenNumber;
+}
+showBoilingTemp();
+
+const showFreezingTemp = () => {
+  boilingTemp.innerHTML = boilingNumber;
+}
+showFreezingTemp();
+
+const showWatergTemp = () => {
+  waterTemp.innerHTML = waterTempNumber;
+}
+showWatergTemp();
+
+const showIfWaterIsFine = () => {
+  if(waterTempNumber > boilingNumber) {
+    boilingMessage.classList.remove('hide');
+  }
+  else if (waterTempNumber < frozenNumber) {
+    freezingMessage.classList.remove('hide');
+  }
+  else {
+    goodTempMessage.classList.remove('hide');
+  }
+}
+setTimeout(showIfWaterIsFine, 3000);
